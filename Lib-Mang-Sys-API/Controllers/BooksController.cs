faa285ea4fdace803ac63050a04bf074cbfd51cf
PC172\Administrator <prasad.suryavanshi@rheal.com>
2025-08-02 19:24:49 +0530

    Add/edit done

--- Lib-Mang-Sys-API/Controllers/BooksController.cs
+++ Lib-Mang-Sys-API/Controllers/BooksController.cs
@@ -4,6 +4,11 @@ using System.Net.Http;
 using System.Web.Http;
 using Library_Mangament_System_API_ClassLibrary.DAL;
 using Library_Mangament_System_API_ClassLibrary.Models;
+using LibraryManagementSystemClassLibrary;
+using Books = Library_Mangament_System_API_ClassLibrary.DAL.Books;
+using BooksModel = Library_Mangament_System_API_ClassLibrary.Models.BooksModel;
+using Languages = Library_Mangament_System_API_ClassLibrary.DAL.Languages;
+using Publishers = Library_Mangament_System_API_ClassLibrary.DAL.Publishers;
 
 namespace Lib_Mang_Sys_API.Controllers
 {
@@ -52,43 +57,82 @@ namespace Lib_Mang_Sys_API.Controllers
         }
 
 
-            [HttpPost]
-            public HttpResponseMessage AddEditBook(BooksModel model)
+        [HttpPost]
+        public HttpResponseMessage AddEditBook(BooksModel model)
+        {
+            try
             {
-                try
+                Books objBookDal = new Books
                 {
-                    Books objBookDal = new Books
-                    {
-                        BookId = model.BookId,
-                        BookName = model.BookName,
-                        PublisherId = model.PublisherId,
-                        LanguageId = model.LanguageId,
-                        Cost = model.Cost,
-                        Pages = model.Pages,
-                        TotalQuantity = model.TotalQuantity,
-                        AvailableQuantity = model.AvailableQuantity,
-                        IsActive = model.IsActive,
-                        CreatedBy = model.CreatedBy
-                    };
+                    BookId = model.BookId,
+                    BookName = model.BookName,
+                    PublisherId = model.PublisherId,
+                    LanguageId = model.LanguageId,
+                    Cost = model.Cost,
+                    Pages = model.Pages,
+                    TotalQuantity = model.TotalQuantity,
+                    AvailableQuantity = model.AvailableQuantity,
+                    IsActive = model.IsActive,
+                    CreatedBy = model.CreatedBy
+                };
 
-                    bool isSuccess = objBookDal.Save();
+                bool isSuccess = objBookDal.Save();
 
-                    if (isSuccess && objBookDal.BookId > 0)
-                    {
-                        string msg = model.BookId == 0 ? "Book inserted successfully." : "Book updated successfully.";
-                        return Request.CreateResponse(HttpStatusCode.OK, new { message = msg });
-                    }
-                    else
-                    {
-                        return Request.CreateResponse(HttpStatusCode.BadRequest, new { message = "Book Operation Failed." });
-                    }
-                }
-                catch (Exception ex)
+                if (isSuccess && objBookDal.BookId > 0)
                 {
-                    Errors.LogErrorToFile(ex);
-                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { message = "Something went wrong." });
+                    string msg = model.BookId == 0 ? "Book inserted successfully." : "Book updated successfully.";
+                    return Request.CreateResponse(HttpStatusCode.OK, new { message = msg });
                 }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { message = "Book Operation Failed." });
+                }
+            }
+            catch (Exception ex)
+            {
+                Errors.LogErrorToFile(ex);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { message = "Something went wrong." });
+            }
         }
+
+
+        [HttpPost]
+        public HttpResponseMessage GetEditBook(BooksModel model)
+        {
+            HttpResponseMessage result = null;
+
+            try
+            {
+
+                Books objBookDal = new Books();
+                objBookDal.BookId = model.BookId;
+
+                objBookDal.Load();
+
+                model.BookId = objBookDal.BookId;
+                model.BookName = objBookDal.BookName;
+                model.PublisherId = objBookDal.PublisherId;
+                model.LanguageId = objBookDal.LanguageId;
+                model.Cost = objBookDal.Cost;
+                model.Pages = objBookDal.Pages;
+                model.TotalQuantity = objBookDal.TotalQuantity;
+                model.AvailableQuantity = objBookDal.AvailableQuantity;
+                model.IsActive = objBookDal.IsActive;
+                model.CreatedBy = objBookDal.CreatedBy;
+                model.CreatedOn = objBookDal.CreatedOn;
+                model.ModifiedBy = objBookDal.ModifiedBy;
+                model.ModifiedOn = objBookDal.ModifiedOn;
+                result = Request.CreateResponse(HttpStatusCode.OK, model);
+            }
+            catch (Exception ex)
+            {
+                Errors.LogErrorToFile(ex);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { message = "Something went wrong." });
+            }
+            return result;
+        }
+
+
 
     }
 }
